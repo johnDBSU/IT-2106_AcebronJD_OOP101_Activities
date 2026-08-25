04f276e88b7d9d243a4bd9561019e00cbfb63c09
johnDBSU <25-06694@g.batstate-u.edu.ph>
2026-08-25 08:56:26 +0800

    test

--- test.java
+++ test.java
@@ -1,11 +1,17 @@
 class Sample1{
 	public static void main(String[]args){
-		System.out.println("Hello Ma'am Fatima!");
-		System.out.println("This is OOP101: Object Oriented Programming!");
-        
-		System.out.println();
+		// System.out.println("Hello Ma'am Fatima!");
+		// System.out.println("This is OOP101: Object Oriented Programming!");
 
-		System.out.print("Hello Ma'am Fatima!");
-		System.out.print("This is OOP101: Object Oriented Programming!");
+		// System.out.println();
+
+		// System.out.print("Hello Ma'am Fatima!");
+		// System.out.print("This is OOP101: Object Oriented Programming!");
+        double num1 = 17.4;
+        float num2 = 93.5f;
+        int num3 = 103;
+        System.out.println("num1: " + num1);
+        System.out.println("num2: " + num2);
+        System.out.println("num3: " + num3);
 	}
 }
