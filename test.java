d31c947f387e52cd3e86fed42eed533c3340e9a9
johnDBSU <25-06694@g.batstate-u.edu.ph>
2026-08-25 08:29:32 +0800

    test file

--- test.java
+++ test.java
@@ -2,7 +2,9 @@ class Sample1{
 	public static void main(String[]args){
 		System.out.println("Hello Ma'am Fatima!");
 		System.out.println("This is OOP101: Object Oriented Programming!");
+        
 		System.out.println();
+
 		System.out.print("Hello Ma'am Fatima!");
 		System.out.print("This is OOP101: Object Oriented Programming!");
 	}
