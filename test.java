d7159f34e04eeb3b2e2e7d44998b0a98d7de8ccd
johnDBSU <25-06694@g.batstate-u.edu.ph>
2026-08-26 21:34:22 -0700

    lol

--- test.java
+++ test.java
@@ -7,11 +7,11 @@ class Sample1{
 
 		// System.out.print("Hello Ma'am Fatima!");
 		// System.out.print("This is OOP101: Object Oriented Programming!");
-        double num1 = 17.4;
-        float num2 = 93.5f;
-        int num3 = 103;
-        System.out.println("num1: " + num1);
-        System.out.println("num2: " + num2);
-        System.out.println("num3: " + num3);
+        // double num1 = 17.4;
+        // float num2 = 93.5f;
+        // int num3 = 103;
+        // System.out.println("num1: " + num1);
+        // System.out.println("num2: " + num2);
+        // System.out.println("num3: " + num3);
 	}
 }
